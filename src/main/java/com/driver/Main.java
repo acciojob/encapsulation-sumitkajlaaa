29852bac6656c044b9884568ae5f75a44e675a15
sumit <sumitkajla47@gmail.com>
2024-06-29 20:39:04 +0530

    commit after doing all tasks

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String []args){
+      RWOnly rwOnly = new RWOnly();
+//      rwOnly.name = "newName";
+//      System.out.println(rwOnly.name);
+
+      //java: name has private access in com.driver.RWOnly
+
+      rwOnly.setName("Sumit");
+
+      System.out.println(rwOnly.getName());
+  }
 }
